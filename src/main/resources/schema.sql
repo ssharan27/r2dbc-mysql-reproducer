26bbf9608136479d7df222a9752fad2bb91f52f1
shriyam.sharan <shriyam.sharan@99acres.com>
2023-06-16 12:36:00 +0530

    modified db initializer queries

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
---create table test( id int PRIMARY KEY, name varchar(255), age int );
---insert into test.test (name, age) values('abc', 21);
+create table test( id int PRIMARY KEY, name varchar(255), age int );
+insert into test.test (name, age) values('abc', 21);
 insert into test.test (name, age) values('abd', 25);
 insert into test.test (name, age) values('aba', 24);
 insert into test.test (name, age) values('abe', 23);
