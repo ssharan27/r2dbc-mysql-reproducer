70a73914950a904e30b58e9b26f2e6d3c16a84b2
jchrys <jchrys@me.com>
2023-06-16 20:02:04 +0900

    idempotent schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,5 @@
-create table test( id int PRIMARY KEY, name varchar(255), age int );
+drop table if exists test;
+create table test( id int AUTO_INCREMENT PRIMARY KEY , name varchar(255), age int );
 insert into test.test (name, age) values('abc', 21);
 insert into test.test (name, age) values('abd', 25);
 insert into test.test (name, age) values('aba', 24);
